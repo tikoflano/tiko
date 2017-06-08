82d37e29b2501158355e99dd31397c9b0641552b
Alvaro Flaño <tikoflano@gmail.com>
2017-06-08 15:41:17 -0300

    Se devuelve a los parametros standard

--- game/game.js
+++ game/game.js
@@ -2,7 +2,7 @@ var app = angular.module("game", ["ngMessages"]);
 app.config(function($locationProvider) { $locationProvider.html5Mode(true); });
 app.constant("Config", {
     debug: false,
-    board : {width: 2, height: 2},
+    board : {width: 5, height: 5},
     figure : {size: 4},
     player : {hand_size: 3, player_cards: 6, board: {width: 5, height: 5}}
 });
