2130e06aee3a7909a37db31b56bff79ee2b775dc
Alvaro Flaño <tikoflano@gmail.com>
2017-06-08 14:12:33 -0300

    Corregida la carta de "lanzar 3 dados" para que no se use con el tablero vacio

--- game/game.js
+++ game/game.js
@@ -3,5 +3,5 @@ app.config(function($locationProvider) { $locationProvider.html5Mode(true); });
 app.constant("Config", {
     board : {width: 5, height: 5},
     figure : {size: 4},
-    player : {hand_size: 3, player_cards: 2, board: {width: 5, height: 5}}
+    player : {hand_size: 3, player_cards: 6, board: {width: 5, height: 5}}
 });
